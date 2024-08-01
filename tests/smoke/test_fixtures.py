02a95e371fba559687549bf17540e9618459ad36
Dayenne Souza <ddesouza@microsoft.com>
2024-08-01 18:00:46 -0300

    remove connection string

--- tests/smoke/test_fixtures.py
+++ tests/smoke/test_fixtures.py
@@ -245,9 +245,6 @@ class TestIndexer:
         os.environ,
         {
             **os.environ,
-            "BLOB_STORAGE_CONNECTION_STRING": os.getenv(
-                "GRAPHRAG_CACHE_CONNECTION_STRING", None
-            ),
             "LOCAL_BLOB_STORAGE_CONNECTION_STRING": WELL_KNOWN_AZURITE_CONNECTION_STRING,
             "GRAPHRAG_CHUNK_SIZE": "1200",
             "GRAPHRAG_CHUNK_OVERLAP": "0",
